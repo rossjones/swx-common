ecfa2958a3d79c7a43df9c5fe9634c53c6ffb2f3
Ross Jones <ross@servercode.co.uk>
2013-07-23 22:00:06 +0100

    Initial

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from swx.common import __version__
+
+setup(
+    name='swx-common',
+    version=__version__,
+    long_description="""\
+""",
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    namespace_packages=['swx.common'],
+    zip_safe=False,
+    author='Ross Jones',
+    author_email='ross@mailbolt.com',
+    license='AGPL',
+    url='',
+    description='Common code shared across SWX components',
+    keywords='datastore scraping',
+    install_requires=[
+        "ConcurrentLogHandler==0.8.3"
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    package_data={'ckan': ['i18n/*/LC_MESSAGES/*.mo']},
+    entry_points="""
+""",
+    test_suite = 'nose.collector',
+)
